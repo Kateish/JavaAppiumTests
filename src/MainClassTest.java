2d1727d487653f18fe18d318b340fe43a8033839
Kateish <46592403+Kateish@users.noreply.github.com>
2019-11-07 17:44:08 +0700

    test2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -5,14 +5,23 @@ public class MainClassTest {
     MainClass mainClass = new MainClass();
     int actualInt = mainClass.getLocalNumber();
     int expectedInt = 14;
+    int numberFromTask2 = mainClass.getClassNumber();
+
     @Test
-    public void testGetLocalNumber(){
+    public void testGetLocalNumber() {
         Assert.assertEquals("ints are not equal", expectedInt, actualInt);
     }
+
     @Test
-    public void failingTest(){
-       this.expectedInt = 8;
-       testGetLocalNumber();
+    public void failingTest() {
+        this.expectedInt = 8;
+        testGetLocalNumber();
     }
 
+    //the test below is expected to fail
+    @Test
+    public void testGetClassNumber() {
+
+        Assert.assertTrue("the value is less than 45", numberFromTask2 > 45);
+    }
 }
